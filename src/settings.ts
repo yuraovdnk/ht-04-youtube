67bc31c9f0f325803c2aeb930c8c1945758bda9f
yuraovdnk <yuraovdnk1999@ukr.net>
2022-06-09 18:58:29 +0300

    fix mongo uri

--- src/settings.ts
+++ src/settings.ts
@@ -1,4 +1,4 @@
 export const settings = {
     JWT_SECRET: process.env.JWT_SECRET || "123",
-    mongoUri: process.env.MongoURI  || 'mongodb+srv://yuraovdnk:Cxo44f0Ifmg3l77X@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority'
+    mongoUri: process.env.MongoURI  || 'mongodb+srv://yuraovdnk:ovdey1970@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority'
 }
