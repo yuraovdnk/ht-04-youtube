8b3dede9560c3816d46209411b15975b69998252
yuraovdnk <yuraovdnk1999@ukr.net>
2022-06-09 18:53:35 +0300

    fix mongo uri

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,13 +1,10 @@
 import {MongoClient} from "mongodb";
 import {bloggerType, CommentType, postsType, UserType} from "./types";
+import {settings} from "../settings";
 
 // const t = || "mongodb://localhost:27017"
-let mongoUri = process.env.MongoURI  || 'mongodb+srv://yuraovdnk:Cxo44f0Ifmg3l77X@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority'
-// if(!mongoUri){
-//     mongoUri = 'mongodb+srv://yuraovdnk:sZ6ZJHPQBQIBzdU3@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority'
-// }
 
-const client = new MongoClient(mongoUri)
+const client = new MongoClient(settings.mongoUri)
 const db = client.db('youtube')
 export const bloggerCollection = db.collection<bloggerType>("bloggers")
 export const postsCollection = db.collection<postsType>("posts")
@@ -20,7 +17,7 @@ export async function runDb() {
         console.log("Succesfully connected to db")
 
     } catch (e) {
-        console.log("Not connected to db")
+        console.log("Not connected to db", e)
         await client.close()
     }
 }
--- src/settings.ts
+++ src/settings.ts
@@ -1,3 +1,4 @@
 export const settings = {
-    JWT_SECRET: process.env.JWT_SECRET || "123"
+    JWT_SECRET: process.env.JWT_SECRET || "123",
+    mongoUri: process.env.MongoURI  || 'mongodb+srv://yuraovdnk:Cxo44f0Ifmg3l77X@cluster0.sbzbx.mongodb.net/?retryWrites=true&w=majority'
 }
